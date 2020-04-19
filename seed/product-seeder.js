77c344893f0990d4d09ed176272973bc5923fff1
Andreas Krogh <av.krogh@gmail.com>
2020-04-19 11:19:11 +0300

    Første push til projektet

---
+++ seed/product-seeder.js
@@ -0,0 +1,47 @@
+var Product = require('../models/product');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/shopping', { useNewUrlParser: true, useUnifiedTopology: true });
+
+var products = [
+    new Product({
+       imagePath: 'https://i.pinimg.com/474x/1b/73/80/1b738096a305befa82f3550519979ebe.jpg',
+        title: 'Brune ris',
+        description: 'Elsker elsker elsker brune ris',
+        price: 10
+    }),
+    new Product({
+        imagePath: 'https://www.olgainthekitchen.com/wp-content/uploads/2019/08/Perfect-White-Rice-Recipe-1-500x375.jpg',
+        title: 'Hvide ris',
+        description: 'Hvide ris MÅMS',
+        price: 10
+    }),
+    new Product({
+        imagePath: 'https://feed-your-sole.com/wp-content/uploads/2019/12/Coconut-Basmati-Wild-Rice-800x687.png',
+        title: 'Vilde ris',
+        description: 'Wild rice watch out',
+        price: 10
+    }),
+    new Product({
+        imagePath: 'https://images.interactives.dk/istock-184842128-ilAFOUXlmHd8wAxofg1LLg.jpg?auto=compress&ch=Width%2CDPR&dpr=2.63&ixjsv=2.2.4&q=38&rect=104%2C0%2C5408%2C3744&w=430',
+        title: 'Sorte ris',
+        description: 'Vidste ikke en gang at der fandtes sorte ris',
+        price: 10
+    })
+];
+
+
+var done = 0;
+for (var i=0; i < products.length; i++) {
+    products[i].save(function (err, result) {
+        done++;
+        if (done === products.length) {
+            exit();
+        }
+    });
+}
+
+function exit() {
+    mongoose.disconnect();
+}
